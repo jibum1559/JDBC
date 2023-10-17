ddaf159ee89387e196c7a9bf589e05d73d16d1e4
jibum1559 <143372311+jibum1559@users.noreply.github.com>
2023-10-17 17:32:15 +0900

    jdbc_select_insert

--- chap1_DB_ORACLE/src/com/kh/db/oraclesample/DBConnection.java
+++ chap1_DB_ORACLE/src/com/kh/db/oraclesample/DBConnection.java
@@ -15,7 +15,8 @@ public class DBConnection {
 		//selectKhcafe();
 		//selectIf();
 		//selectWhere();
-		selectIntWhere();
+		//selectIntWhere();
+		insertBank();
 	}		
 	static void selectBank() {
 		//1. 드라이버 연결 :          Oracle JDBC  드라이버    클래스 이름
@@ -199,6 +200,29 @@ public class DBConnection {
 				
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	static void insertBank() {
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "KHBANK";
+		String password = "KHBANK";
+		try {
+			Connection con = DriverManager.getConnection(url, user, password);
+			String insertQuery = "INSERT INTO BANK (account_id, account_number, account_name, balance, branch_name, last_transaction_date)"
+								+ "VALUES (?, ?, ?, ?, ?, ?)";
+			PreparedStatement insertState = con.prepareStatement(insertQuery);
+			insertState.setInt(1, 11);
+			insertState.setString(2, "1114117721");
+			insertState.setString(3, "김승범");
+			insertState.setDouble(4, 9900.00);
+			insertState.setString(5, "동쪽지점");
+			insertState.setDate(6, Date.valueOf("2023-10-12"));
+			
+			int rowsInsert = insertState.executeUpdate();
+			System.out.println(rowsInsert + "row 추가됨");
+			
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
